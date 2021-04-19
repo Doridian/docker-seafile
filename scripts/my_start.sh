ec48b63a89d9232404465ae117a22359778ef49e
Mark Dietzer <git@doridian.net>
2021-04-18 18:57:16 -0700

    force defaults file load

--- scripts/my_start.sh
+++ scripts/my_start.sh
@@ -2,6 +2,8 @@
 set -eu
 
 export DB_HOST=127.0.0.1
+export DB_MY_CONF='/var/lib/mysql/dotmy.cnf'
+export DB_MY_CONF_ARG="--defaults-file=$DB_MY_CONF"
 
 # Initialize DB if needed
 if [ ! -d /var/lib/mysql/mysql ]
@@ -16,7 +18,7 @@ fi
 timeout 60 /scripts/wait_mysql.sh
 
 # Initialize root PW if needed
-if [ ! -f /var/lib/mysql/dotmy.cnf ]
+if [ ! -f "$DB_MY_CONF" ]
 then
     if [ -z "${DB_ROOT_PASSWD:-}" ]
     then
@@ -24,10 +26,10 @@ then
         export DB_ROOT_PASSWD="$(dd status=none if=/dev/urandom of=/dev/stdout bs=48 count=1 | base64)"
     fi
     mysql mysql -B -e "UPDATE user SET password = PASSWORD('$DB_ROOT_PASSWD') WHERE User = 'root';"
-    echo '[client]' > /var/lib/mysql/dotmy.cnf
-    chmod 600 /var/lib/mysql/dotmy.cnf
-    echo "password=$DB_ROOT_PASSWD" >> /var/lib/mysql/dotmy.cnf
-    mysqladmin flush-privileges
+    echo '[client]' > "$DB_MY_CONF"
+    chmod 600 "$DB_MY_CONF"
+    echo "password=$DB_ROOT_PASSWD" >> "$DB_MY_CONF"
+    mysqladmin "$DB_MY_CONF_ARG" flush-privileges
 fi
 
 export DB_ROOT_PASSWD="$(grep -Po '(?<=password=).+' /root/.my.cnf)"
--- scripts/wait_mysql.sh
+++ scripts/wait_mysql.sh
@@ -2,7 +2,7 @@
 set -eu
 
 echo -n 'Waiting for MySQL...'
-until mysql mysql -B -e 'SELECT 1' &> /dev/null
+until mysql "$DB_MY_CONF_ARG" mysql -B -e 'SELECT 1' &> /dev/null
 do
     echo -n '.'
     sleep 1
