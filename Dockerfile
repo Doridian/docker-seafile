a9989e501f6d5584347dd4a1bf4079ab72003966
Mark Dietzer <git@doridian.net>
2021-04-18 17:10:27 -0700

    autogen root pw if needed. grep password out of my.cnf

--- Dockerfile
+++ Dockerfile
@@ -5,8 +5,15 @@ RUN apt-get update && apt-get install -y wget mariadb-server memcached && apt-ge
 RUN sed -i 's~memcached:11211~127.0.0.1:11211~g' /scripts/bootstrap.py
 RUN sed -i 's~%.%.%.%~127.0.0.1~g' /scripts/bootstrap.py
 
+RUN rm -rf /var/lib/mysql/*
+
 RUN ln -s /var/lib/mysql/dotmy.cnf /root/.my.cnf
 VOLUME "/var/lib/mysql"
+
+RUN mkdir -p /etc/service/mysqld /etc/service/memcached
+COPY services/mysqld.sh /etc/service/mysqld/run
+COPY services/memcached.sh /etc/service/memcached/run
+RUN chmod 755 /etc/service/*/run
 
 COPY my_start.sh /sbin/my_start.sh
 RUN chmod 755 /sbin/my_start.sh
--- my_start.sh
+++ my_start.sh
@@ -3,24 +3,29 @@ set -eu
 
 export DB_HOST=127.0.0.1
 
-if [ ! -d /var/lib/mysql/seafile_db ]
+if [ ! -d /var/lib/mysql/mysql ]
 then
-    service mysql stop
-    rm -rf /var/lib/mysql/*
+    sv stop mysqld
     chown -R mysql:mysql /var/lib/mysql
     mysql_install_db --user=mysql --auth-root-authentication-method=normal --datadir=/var/lib/mysql
+    sv start mysqld
 fi
-
-service mysql start
 
 if [ ! -f /var/lib/mysql/dotmy.cnf ]
 then
+    if [ -z "${DB_ROOT_PASSWD:-}" ]
+    then
+        echo 'No DB_ROOT_PASSWD given, generating one randomly...'
+        export DB_ROOT_PASSWD="$(dd status=none if=/dev/urandom of=/dev/stdout bs=48 count=1 | base64)"
+    fi
     mysql mysql -B -e "UPDATE user SET password = PASSWORD('$DB_ROOT_PASSWD') WHERE User = 'root';"
     echo '[mysql]' > /var/lib/mysql/dotmy.cnf
+    chmod 600 /var/lib/mysql/dotmy.cnf
     echo "password=$DB_ROOT_PASSWD" >> /var/lib/mysql/dotmy.cnf
     mysqladmin flush-privileges
 fi
 
-echo 'Giving control to /scripts/start.py'
+export DB_ROOT_PASSWD="$(grep -Po '(?<=password=).+' /root/.my.cnf)"
 
+echo 'Giving control to /scripts/start.py'
 exec /scripts/start.py
